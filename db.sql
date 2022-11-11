1f935dea5201d9b8ef1276e8a6e903bf31595f6d
micaelavera <micavera2010@hotmail.com>
2022-11-11 01:25:20 -0300

    add database and tables

--- db.sql
+++ db.sql
@@ -1,1 +1,19 @@
+-- DROP DATABASE IF EXISTS db;
 
+CREATE DATABASE db;
+USE db;
+
+CREATE TABLE vehiculo (idVehiculo INTEGER NOT NULL, 
+                       patente VARCHAR(10), 
+                       anio DATE);
+
+CREATE TABLE marcas (idMarca INTEGER NOT NULL, 
+                    nombre VARCHAR(40));
+
+CREATE TABLE modelos (idModelo INTEGER NOT NULL, 
+                     nombre VARCHAR(40));
+
+
+CREATE TABLE cliente (idCliente INTEGER NOT NULL,
+                     nombre VARCHAR(40), 
+                     apellido VARCHAR(40));
